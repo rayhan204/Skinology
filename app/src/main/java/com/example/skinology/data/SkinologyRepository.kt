91e5f2776d53af738aea41d76e16485b1bd4c047
Rayhan <rgozali48@gmail.com>
2024-11-25 05:04:10 +0700

    logica camera, add logo

--- app/src/main/java/com/example/skinology/data/SkinologyRepository.kt
+++ app/src/main/java/com/example/skinology/data/SkinologyRepository.kt
@@ -1,4 +1,18 @@
 package com.example.skinology.data
 
-class SkinologyRepository {
+import com.example.skinology.data.local.room.SkinologyDao
+
+class SkinologyRepository private constructor(
+    private val skinologyDao: SkinologyDao
+){
+
+    companion object {
+        @Volatile
+        private var instance: SkinologyRepository? = null
+
+        fun getInstance(skinologyDao: SkinologyDao): SkinologyRepository =
+            instance ?: synchronized(this) {
+                instance ?: SkinologyRepository(skinologyDao)
+            }.also { instance = it }
+    }
 }
--- app/src/main/java/com/example/skinology/ui/result/ResultActivity.kt
+++ app/src/main/java/com/example/skinology/ui/result/ResultActivity.kt
@@ -3,10 +3,13 @@ package com.example.skinology.ui.result
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.skinology.R
+import com.example.skinology.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityResultBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
