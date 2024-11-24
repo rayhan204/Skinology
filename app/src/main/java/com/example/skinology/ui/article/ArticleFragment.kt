58627be3805cfa963ceed3a61d94b10a41f9f335
cintia <cintiauml@gmail.com>
2024-11-24 15:30:45 +0700

    update article dan camera x

--- app/src/main/java/com/example/skinology/ui/article/ArticleFragment.kt
+++ app/src/main/java/com/example/skinology/ui/article/ArticleFragment.kt
@@ -1,5 +1,6 @@
 package com.example.skinology.ui.article
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,9 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.skinology.databinding.FragmentArticleBinding
+import com.example.skinology.databinding.FragmentCameraBinding
+import com.example.skinology.ui.article2.Articel2Activity
+import com.example.skinology.ui.cameraX.CameraX
 
 class ArticleFragment : Fragment() {
 
@@ -29,6 +33,10 @@ class ArticleFragment : Fragment() {
 
     private fun setupUI() {
         binding.progressBar.visibility = View.GONE
+        binding.buttonDry.setOnClickListener {
+            val intent = Intent(requireContext(), Articel2Activity::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/skinology/ui/camera/CameraFragment.kt
+++ app/src/main/java/com/example/skinology/ui/camera/CameraFragment.kt
@@ -1,5 +1,6 @@
 package com.example.skinology.ui.camera
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,12 +8,14 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.example.skinology.R
 import com.example.skinology.databinding.FragmentCameraBinding
+import com.example.skinology.ui.cameraX.CameraX
 
 class CameraFragment : Fragment() {
 
     private var _binding: FragmentCameraBinding? = null
-
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -26,15 +29,21 @@ class CameraFragment : Fragment() {
         _binding = FragmentCameraBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textCamera
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        setupUI()
         return root
+    }
+
+    private fun setupUI() {
+        binding.progressBar.visibility = View.GONE
+        binding.buttonCamera.setOnClickListener {
+            val intent = Intent(requireContext(), CameraX::class.java)
+            startActivity(intent)
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 }
