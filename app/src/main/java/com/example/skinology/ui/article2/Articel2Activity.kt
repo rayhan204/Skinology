bfc171c6f864cb3ea8b6e71073c1aaaacabd92d4
cintia <cintiauml@gmail.com>
2024-11-25 20:13:36 +0700

    benerin article

--- app/src/main/java/com/example/skinology/ui/article2/Articel2Activity.kt
+++ app/src/main/java/com/example/skinology/ui/article2/Articel2Activity.kt
@@ -15,22 +15,39 @@ import com.example.skinology.databinding.ActivityArticel2Binding
 import com.example.skinology.databinding.ActivityDetailArticelBinding
 import com.example.skinology.databinding.FragmentArticleBinding
 
-class Articel2Activity : AppCompatActivity() {
+class Articel2Activity : AppCompatActivity(), ButtonsFragment.ButtonSelectionListener {
     private lateinit var binding: ActivityArticel2Binding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityArticel2Binding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val selectedButton = intent.getStringExtra("SELECTED_BUTTON")
+        updateTitle(selectedButton)
         binding.topAppBar.setNavigationOnClickListener {
             onBackPressed()
         }
         if (savedInstanceState == null) {
-            val fragment = ButtonsFragment()
+            val fragment = ButtonsFragment.newInstance(selectedButton)
             supportFragmentManager.beginTransaction()
                 .replace(R.id.buttonContainerFragment, fragment)
                 .commit()
         }
     }
 
+    override fun onButtonSelected(buttonType: String) {
+        updateTitle(buttonType)
+    }
+
+    private fun updateTitle(selectedButton: String?) {
+        val title = when (selectedButton) {
+            "DRY" -> "Know Dry Skin"
+            "OILY" -> "Know Oily Skin"
+            "NORMAL" -> "Know Normal Skin"
+            else -> "know your skin"
+        }
+        binding.text2.text = title
+    }
 }
+
