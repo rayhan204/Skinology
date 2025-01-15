a621dcd9f1f2391f84a5d7cb85a6d33f5969fd91
Rayhan <rgozali48@gmail.com>
2025-01-15 12:41:46 +0700

    commit

--- app/src/main/java/com/example/skinology/ui/detailarticle/DetailViewModel.kt
+++ app/src/main/java/com/example/skinology/ui/detailarticle/DetailViewModel.kt
@@ -8,6 +8,7 @@ import com.example.skinology.data.local.entity.ArticleEntity
 import com.example.skinology.data.Result
 
 class DetailViewModel(private val repository: SkinologyRepository): ViewModel() {
+
     fun getArticleId(articleId: String): LiveData<Result<ArticleEntity>> {
         Log.d("DetailViewModel", "Fetching article with ID: $articleId")
         return repository.getArticleId(articleId)
